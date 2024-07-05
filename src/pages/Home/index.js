31e6229fbc5cc86e6c357aa24f7bbb9f7e04458a
Teen-raven <bakatchii@gmail.com>
2024-07-05 16:15:01 +0200

    Update index.js

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -122,7 +122,7 @@ const Page = () => {
     <footer className="row">
       <div className="col presta">
         <h3>Notre derniére prestation</h3>
-  
+
         {last &&(
           <EventCard
           imageSrc={last?.cover}
